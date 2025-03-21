1631674ec0cdb6da770b2c7fcbc3c72a45013dba
Esli Silva <eslih@pm.me>
2025-03-21 13:46:17 -0300

    imdb to libremdb

--- use-redirect-imdb.js
+++ use-redirect-imdb.js
@@ -1,25 +1,24 @@
 (() => {
-    "use strict";
+  "use strict";
 
-    function redirectImdbLinks() {
-        document.querySelectorAll('a[href]').forEach(function(link) {
-            try {
-                const url = new URL(link.href);
-                if (url.hostname === 'www.imdb.com' || url.hostname === 'imdb.com') {
-                    url.hostname = 'libremdb.iket.me';
-                    link.href = url.toString();
-                }
-            } catch (e) {
-                // Ignore invalid URLs
-            }
-        });
-    }
+  function redirectImdbLinks() {
+    document.querySelectorAll("a[href]").forEach(function (link) {
+      try {
+        const url = new URL(link.href);
+        if (url.hostname === "www.imdb.com" || url.hostname === "imdb.com") {
+          url.hostname = "libremdb.iket.me";
+          link.href = url.toString();
+        }
+      } catch (e) {
+        // Ignore invalid URLs
+      }
+    });
+  }
 
-    // redirect
-    redirectImdbLinks();
+  // redirect
+  redirectImdbLinks();
 
-    // DOM and dinamic links
-    const observer = new MutationObserver(redirectImdbLinks);
-    observer.observe(document.body, { childList: true, subtree: true });
-
+  // DOM and dinamic links
+  const observer = new MutationObserver(redirectImdbLinks);
+  observer.observe(document.body, { childList: true, subtree: true });
 })();
