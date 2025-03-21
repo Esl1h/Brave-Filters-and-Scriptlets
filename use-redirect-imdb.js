dbc00950709c9438d6e331a9e761e34d1fd9d29b
Esli Silva <eslih@pm.me>
2025-03-21 13:45:03 -0300

    imdb to libremdb

---
+++ use-redirect-imdb.js
@@ -0,0 +1,25 @@
+(() => {
+    "use strict";
+
+    function redirectImdbLinks() {
+        document.querySelectorAll('a[href]').forEach(function(link) {
+            try {
+                const url = new URL(link.href);
+                if (url.hostname === 'www.imdb.com' || url.hostname === 'imdb.com') {
+                    url.hostname = 'libremdb.iket.me';
+                    link.href = url.toString();
+                }
+            } catch (e) {
+                // Ignore invalid URLs
+            }
+        });
+    }
+
+    // redirect
+    redirectImdbLinks();
+
+    // DOM and dinamic links
+    const observer = new MutationObserver(redirectImdbLinks);
+    observer.observe(document.body, { childList: true, subtree: true });
+
+})();
